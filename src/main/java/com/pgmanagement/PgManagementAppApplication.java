6bd2cbc7e0bd4f819f1a6d7439a9cbc1e0c73a7e
Gopal Raju <dommarajugopal6@gmail.com>
2024-06-20 11:35:30 +0530

    [Initial Code Setup]

---
+++ src/main/java/com/pgmanagement/PgManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.pgmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgManagementAppApplication.class, args);
+	}
+
+}
